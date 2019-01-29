7b655cde2f8687aa2545c364ce70216f60a70821
datacanuck <mcgee.p@gmail.com>
2019-01-29 12:54:32 -0800

    Update

--- plot4.R
+++ plot4.R
@@ -14,14 +14,14 @@ plot4 <- function(){
     png(filename = "plot4.png")
     par(mfrow=c(2,2))
     #Plot #1
-    plot(plotdata$DateTime, plotdata$Global_active_power, ylab = "Global Active Power (Kilowatts)", type = "l")
+    plot(plotdata$DateTime, plotdata$Global_active_power, xlab = "", ylab = "Global Active Power (Kilowatts)", type = "l")
     #Plot #2
     plot(plotdata$DateTime, plotdata$Voltage, xlab = "Datetime", ylab = "Voltage", type = "l")
     #Plot #3
     plot(plotdata$DateTime, plotdata$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
     lines(plotdata$DateTime, plotdata$Sub_metering_2, type = "l", col = "red", xlab = "", ylab = "Energy sub metering")
     lines(plotdata$DateTime, plotdata$Sub_metering_3, type = "l", col = "blue", xlab = "", ylab = "Energy sub metering")
-    legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2)
     #Plot #4
     plot(plotdata$DateTime, plotdata$Global_reactive_power, xlab = "Datetime", ylab = "Global_Reactive_Power", type = "l")
     dev.off()
